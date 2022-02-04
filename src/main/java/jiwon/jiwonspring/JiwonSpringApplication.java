60bda516fe50e4d83aad85bb0ed047fd3363d2d2
JJiWoNing <jjiwoning12@gmail.com>
2022-02-04 15:18:51 +0900

    Initial commit

---
+++ src/main/java/jiwon/jiwonspring/JiwonSpringApplication.java
@@ -0,0 +1,13 @@
+package jiwon.jiwonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiwonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiwonSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jiwon/jiwonspring/JiwonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwon.jiwonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiwonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
