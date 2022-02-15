1d38899c6ffffd5860ef8d2a92a67985cd9990c4
JJiWoNing <jjiwoning12@gmail.com>
2022-02-15 14:19:37 +0900

    2/15_Study

--- src/main/java/jiwon/jiwonspring/controller/MemberController.java
+++ src/main/java/jiwon/jiwonspring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package jiwon.jiwonspring.controller;
 
+import jiwon.jiwonspring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import jiwon.jiwonspring.services.MemberService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 이거를 적으면 스프링 컨테이너에 아래의 클래스 객체를 생성해서 스프링빈으로 관리한다.
 public class MemberController {
@@ -16,5 +22,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new") // 조회할 때 쓴다.
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") // 데이터를 전달할때 쓴다.
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 
 }
