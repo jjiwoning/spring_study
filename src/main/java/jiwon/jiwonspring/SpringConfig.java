58aafc5f6aface60ffb675dc70d2854f6ebe626e
JJiWoNing <jjiwoning12@gmail.com>
2022-02-18 00:10:34 +0900

    2/17_Study

--- src/main/java/jiwon/jiwonspring/SpringConfig.java
+++ src/main/java/jiwon/jiwonspring/SpringConfig.java
@@ -1,15 +1,26 @@
 package jiwon.jiwonspring;
 
+import jiwon.jiwonspring.repository.JdbcMemberRepository;
 import jiwon.jiwonspring.repository.MemberRepository;
 import jiwon.jiwonspring.repository.MemoryMemberRepository;
 import jiwon.jiwonspring.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바에서 직접 스프링 빈 등록하는 방법
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
     }
 }
